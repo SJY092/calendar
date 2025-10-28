74cd4f0372c04c7a09b86c567cbe0429e3fd49d0
SJY092 <11110101@gm.chihlee.edu.tw>
2025-10-29 00:10:06 +0800

    Update APIconfig.js

--- APIconfig.js
+++ APIconfig.js
@@ -2,6 +2,7 @@
 // Supabase 設定
 
 export const SUPABASE_URL = 'https://rygawaqskvijgaciinzc.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_QUP9QX6xDzzSLY7NrCCThA_rUw7g55E';//待確認
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5Z2F3YXFza3ZpamdhY2lpbnpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0MTU2OTEsImV4cCI6MjA3NDk5MTY5MX0.6NKKuzDkHd0Pkxm9uYoODmkpEVw6wHYuf_SFV9M9af4';//待確認
+
 
 
